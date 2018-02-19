fdb3321404b5c36b08aaeb217c7e4454ee89b1cf
nicolas <nicolas.des@icloud.com>
2018-02-19 20:58:15 +0100

    ok

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Bienvenue dans la base de données de la NASA !</h1>
+
+<h3>Entrez le mot de passe :</h3>
+
+<form method="post" action="codes.php">
+	<input type="password" name="password">
+	<input type="submit" value="Valider">
+</form>
